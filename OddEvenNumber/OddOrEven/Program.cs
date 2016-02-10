00e34e306a56009c1ac120f68724ea1f15428a44
NickyDavid <nickyhrdavid@gmaill.com>
2016-02-10 15:15:24 +0000

    OddOrEven program.cs

---
+++ OddEvenNumber/OddOrEven/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * C# Program to Check whether the Entered Number is Even or Odd
+ */
+
+using System;
+
+namespace OddOrEven
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.Write("Enter a Number : ");
+			var i = int.Parse(Console.ReadLine());
+			if (i % 2 == 0)
+			{
+				Console.Write("Entered Number is an Even Number");
+				Console.Read();
+			}
+			else
+			{
+				Console.Write("Entered Number is an Odd Number");
+				Console.Read();
+			}
+		}
+	}
+}
