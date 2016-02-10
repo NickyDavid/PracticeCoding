4707d7eb69a9f8cfcfca3d4c3766e1bc2fdb9f73
NickyDavid <nickyhrdavid@gmaill.com>
2016-02-10 15:17:03 +0000

    Added OddOrEven, SumOfDigits, SwapTwoNumbers

---
+++ OddEvenNumber/SumOfDigits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sum = 0;
+			Console.WriteLine("Enter a Number : ");
+			var num = int.Parse(Console.ReadLine());
+			while (num != 0)
+			{
+				var r = num % 10;
+				num = num / 10;
+				sum = sum + r;
+			}
+			Console.WriteLine("Sum of Digits of the Number : " + sum);
+			Console.ReadLine();
+
+		}
+	}
+}
